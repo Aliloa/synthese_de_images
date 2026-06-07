8c38d001beba750ae99e16aa8a18e8d8375e4748
Yasmineboire <yasmine.boire@gmail.com>
2026-06-07 20:59:10 +0200

    gare style chalet

--- TD_the_train/objects/draw_station.cpp
+++ TD_the_train/objects/draw_station.cpp
@@ -3,29 +3,26 @@
 
 static IndexedMesh *murs = nullptr;
 static StandardMesh *toit = nullptr;
-static IndexedMesh *quai = nullptr;
 static IndexedMesh *cheminee = nullptr;
 static IndexedMesh *pancarte = nullptr;
 static IndexedMesh *poteau = nullptr;
-static IndexedMesh *avancee = nullptr;
+static IndexedMesh *porte = nullptr;
 
 void initStation()
 {
-  murs = basicCube(2.0f);
-  toit = basicCone(0.8f, 1.5f);
-  quai = basicCylinder(1.0f, 1.0f);
-  cheminee = basicCylinder(1.0f, 1.0f);
+  murs = basicCube(1.5f);
+  toit = basicCone(0.8f, 1.0f);
+  cheminee = basicCube(1.0f);
   pancarte = basicCube(1.0f);
   poteau = basicCylinder(1.0f, 1.0f);
-  avancee = basicCube(1.0f);
+  porte = basicCube(1.0f);
 
   murs->createVAO();
   toit->createVAO();
-  quai->createVAO();
   cheminee->createVAO();
   pancarte->createVAO();
   poteau->createVAO();
-  avancee->createVAO();
+  porte->createVAO();
 }
 
 void drawStation()
@@ -37,16 +34,6 @@ void drawStation()
 
   myEngine.mvMatrixStack.pushMatrix();
   myEngine.mvMatrixStack.addTranslation({baseX, baseY + 10.f, 0.f});
-
-  // Quai
-  myEngine.mvMatrixStack.pushMatrix();
-  myEngine.setFlatColor(0.75f, 0.75f, 0.72f);
-  myEngine.mvMatrixStack.addTranslation({5.f, 5.f, 0.f});
-  myEngine.mvMatrixStack.addRotation(M_PI, {0, 1, 1});
-  myEngine.mvMatrixStack.addHomothety({10.f, 0.3f, 10.f});
-  myEngine.updateMvMatrix();
-  quai->draw();
-  myEngine.mvMatrixStack.popMatrix();
 
   // Bâtiment principal
   myEngine.mvMatrixStack.pushMatrix();
@@ -60,8 +47,8 @@ void drawStation()
   // Toit chalet
   myEngine.mvMatrixStack.pushMatrix();
   myEngine.setFlatColor(0.25f, 0.15f, 0.05f);
-  myEngine.mvMatrixStack.addTranslation({5.f, 5.f, 7.3f});
-  myEngine.mvMatrixStack.addRotation(-M_PI / 2, {-2, 0, 0});
+  myEngine.mvMatrixStack.addTranslation({5.f, 5.f, 5.3f});
+  myEngine.mvMatrixStack.addRotation(-M_PI / 2, {-1, 0, 0});
   myEngine.mvMatrixStack.addHomothety({9.f, 6.f, 5.f});
   myEngine.updateMvMatrix();
   toit->draw();
@@ -70,17 +57,17 @@ void drawStation()
   // Auvent avant
   myEngine.mvMatrixStack.pushMatrix();
   myEngine.setFlatColor(0.25f, 0.15f, 0.05f);
-  myEngine.mvMatrixStack.addTranslation({5.f, 1.5f, 4.5f});
-  myEngine.mvMatrixStack.addHomothety({7.f, 1.5f, 0.3f});
+  myEngine.mvMatrixStack.addTranslation({10.5f, 5.0f, 4.5f});
+  myEngine.mvMatrixStack.addHomothety({2.0f, 5.0f, 0.2f});
   myEngine.updateMvMatrix();
-  avancee->draw();
+  porte->draw();
   myEngine.mvMatrixStack.popMatrix();
 
   // Cheminée
   myEngine.mvMatrixStack.pushMatrix();
   myEngine.setFlatColor(0.4f, 0.25f, 0.1f);
-  myEngine.mvMatrixStack.addTranslation({5.f, 4.f, 9.f});
-  myEngine.mvMatrixStack.addHomothety({0.8f, 2.5f, 0.8f});
+  myEngine.mvMatrixStack.addTranslation({5.0f, 4.0f, 9.0f});
+  myEngine.mvMatrixStack.addHomothety({0.5f, 0.5f, 2.0f});
   myEngine.updateMvMatrix();
   cheminee->draw();
   myEngine.mvMatrixStack.popMatrix();
@@ -88,7 +75,8 @@ void drawStation()
   // Poteau pancarte
   myEngine.mvMatrixStack.pushMatrix();
   myEngine.setFlatColor(0.4f, 0.25f, 0.1f);
-  myEngine.mvMatrixStack.addTranslation({5.f, -1.f, 1.5f});
+  myEngine.mvMatrixStack.addTranslation({10.5f, 11.f, 1.5f});
+  myEngine.mvMatrixStack.addRotation(M_PI / 2, {0, 0, 1});
   myEngine.mvMatrixStack.addHomothety({0.2f, 0.2f, 3.f});
   myEngine.updateMvMatrix();
   poteau->draw();
@@ -97,10 +85,20 @@ void drawStation()
   // Pancarte
   myEngine.mvMatrixStack.pushMatrix();
   myEngine.setFlatColor(0.95f, 0.90f, 0.6f);
-  myEngine.mvMatrixStack.addTranslation({5.f, -1.f, 4.f});
+  myEngine.mvMatrixStack.addTranslation({10.5f, 11.f, 4.f});
+  myEngine.mvMatrixStack.addRotation(M_PI / 2, {0, 0, 1});
   myEngine.mvMatrixStack.addHomothety({3.f, 0.15f, 1.2f});
   myEngine.updateMvMatrix();
   pancarte->draw();
+  myEngine.mvMatrixStack.popMatrix();
+
+  // Porte
+  myEngine.mvMatrixStack.pushMatrix();
+  myEngine.setFlatColor(0.25f, 0.15f, 0.05f);
+  myEngine.mvMatrixStack.addTranslation({10.5f, 5.0f, 1.5f});
+  myEngine.mvMatrixStack.addHomothety({0.1f, 2.0f, 3.5f});
+  myEngine.updateMvMatrix();
+  porte->draw();
   myEngine.mvMatrixStack.popMatrix();
 
   myEngine.mvMatrixStack.popMatrix();
